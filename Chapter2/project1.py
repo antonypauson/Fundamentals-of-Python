db1b5cd785b03a028b4893f2c4df97e58096c280
Antony <13antonysun@gmail.com>
2024-02-12 21:28:48 +0530

    Finished Chapter 2

--- Chapter2/project1.py
+++ Chapter2/project1.py
@@ -1,6 +1,7 @@
 """
 Author: Antony S
 Date: 2024-02-11
+Program Name: Tax Calculator
 The tax calculator program of the case study outputs a floating-point number 
 that might show more than two digits of precision. Use the round function to 
 modify the program to display at most two digits of precision in the output 
